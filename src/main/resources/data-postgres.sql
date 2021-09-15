1c74c1eafb811e558d06fd0fd200d62039df80cb
Lucasrezzaghi <lbrezzaghi@gmail.com>
2021-09-15 11:19:28 +0000

    first commit

---
+++ src/main/resources/data-postgres.sql
@@ -0,0 +1,3 @@
+INSERT INTO pessoas(nome) VALUES('Bratislava');
+INSERT INTO pessoas(nome) VALUES('Budapest');
+INSERT INTO pessoas(nome) VALUES('Prague');
---
+++ src/main/resources/schema-postgres.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS pessoas;
+CREATE TABLE pessoas(id serial PRIMARY KEY, nome VARCHAR(255));
