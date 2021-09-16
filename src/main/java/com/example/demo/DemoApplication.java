c8b1b8aed97e22bc01d37db0ac8c97b2ca318d66
Lucasrezzaghi <lbrezzaghi@gmail.com>
2021-09-16 09:04:31 +0000

    security improvement

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.example")
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer{
+	
+	private static Class applicationClass = DemoApplication.class;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
